54b1f3105b468eb0e730506139e39d3c83e14297
Shashank Verma <shashankv167@gmail.com>
2026-08-18 20:44:26 +0530

    feat: design standard copyright footer presentation subcomponent

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ marginTop: '40px', borderTop: '1px solid #eaeaea', paddingTop: '20px', textAlign: 'center', fontSize: '13px', color: '#888' }}>
+      <p>© 2026 Minerva Shopping Experience. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
